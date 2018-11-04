da5fd0d5724bef0440104ee1cb74a152e0373d1b
dalinhuang99 <dhuan023@gmail.com>
2018-11-04 12:37:14 -0500

    Statements Assessment completed

---
+++ playground/02/compare.py
@@ -0,0 +1,56 @@
+# from random import randint
+
+# mylist = [(1, 2), (3, 4), (5)]
+
+# for i in mylist:
+#     if type(i) is tuple:
+#         print(i)
+#     else:
+#         print("No!")
+
+# # dictionary
+
+# d = {"k1": 1, "k2": 2, "k3": 3}
+
+# for item in d.items():
+#     print(item)
+
+# a = 5
+# while a < 10:
+#     a += 1
+#     print(a)
+
+# print("ggg")
+
+# enumerate
+
+# l1 = [1, 2, 3]
+# l2 = ["a", "b", "c"]
+
+# for i in zip(l1, l2):
+#     print(i)
+
+# l3 = [[1, 2], [2, 3], [4, 5]]
+
+# for a, b in l3:
+#     print(a)
+
+# l1 = [1, 2, 3]
+# l2 = ["az", "ab", "c"]
+
+# for x in zip(l1, l2):
+#     print(x)
+
+# print(list(zip(l1, l2)))
+
+# print("a" in l2)
+
+# d = {"a": 111, "b": 2222}
+
+# print(d.keys(), d.values())
+
+# print(min(l2))
+
+mylist = [x ** 2 for x in [1, 2, 3, 4]]
+
+print(mylist)
